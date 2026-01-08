ceffae9ff8d1c68df80a5ba4c56aa7095d085367
Vasii Raul <vasiiraul01@gmail.com>
2026-01-08 19:24:20 +0200

    js actual

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+let selectedCity;
+const apiKey = "42bf47e055b40c2949c1df28bfe30f74";
+let weatherData;
+
+async function fetchWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ro`;
+
+  try {
+    const response = await fetch(apiUrl);
+    weatherData = await response.json();
+    console.log(weatherData, "date");
+  } catch {
+    console.log("hey am prins eroarea");
+  }
+}
+
+function updateCity(city) {
+  selectedCity = document.querySelector(".current-city");
+  selectedCity.textContent = city;
+  saveToLocalStorage(city);
+  fetchWeatherData(city);
+}
+
+function saveToLocalStorage(city) {
+  localStorage.setItem("selectedCity", city);
+}
+
+function loadFromLocalStorage() {
+  const city = localStorage.getItem("selectedCity");
+  if (city) {
+    updateCity(city);
+  } else {
+    updateCity("București");
+  }
+}
+
+window.onload = function () {
+  loadFromLocalStorage();
+};
 console.log("Salut");
